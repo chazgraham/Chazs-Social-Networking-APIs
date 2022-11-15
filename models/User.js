6fbdd679547b304914d1ce074281fdc2d4b70164
chaz Graham <chazhg80.cg@gmail.com>
2022-11-15 16:10:19 -0600

    created user model

--- models/User.js
+++ models/User.js
@@ -1,2 +1,36 @@
 const { Schema, model } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
+
+const UserSchema = new Schema({
+        userName: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        thoughts: {
+            type: Schema.Types.ObjectId,
+            ref: 'Thoughts'
+        },
+        friends: []
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+UserSchema.virtual('friendsCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
